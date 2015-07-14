4e2f7387c93234bc0e408a4ad3b9d4bf4a111c1f
Roberto Gonzalez <gonzal99@students.rowan.edu>
2015-07-14 10:24:54 -0400

    updated css and js paths

--- projects/index.php
+++ projects/index.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <title>Foundation | Welcome</title>
-    <link rel="stylesheet" href="css/foundation.css"/>
-    <script src="js/vendor/modernizr.js"></script>
+    <link rel="stylesheet" href="../css/foundation.css"/>
+    <script src="../js/vendor/modernizr.js"></script>
 </head>
 <body>
 <nav class="top-bar" data-topbar role="navigation">
@@ -52,15 +52,8 @@
 </nav>
 <br>
 
-<div class="row">
-    <div class="large-12 column">
-        <img src="img/hd1080.png">
-    </div>
-</div>
-<br>
-
-<script src="js/vendor/jquery.js"></script>
-<script src="js/foundation.min.js"></script>
+<script src="../js/vendor/jquery.js"></script>
+<script src="../js/foundation.min.js"></script>
 <script>
     $(document).foundation();
 </script>
