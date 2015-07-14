fd75124f02e2c84f746b5793bcf254b86ef5817c
Roberto Gonzalez <gonzal99@students.rowan.edu>
2015-07-14 09:49:51 -0400

    new menu, projects

--- index.php
+++ index.php
@@ -25,6 +25,18 @@
                     <li><a href="#">Submenu Item 3</a></li>
                 </ul>
             </li>
+            <li class="has-dropdown"><a href="#">Projects</a>
+                <ul class="dropdown">
+                    <?php
+                    $link = mysqli_connect('localhost','public','XZmMSa7fZHfR5a8C','main') or die("error ". mysqli_error($link));
+                    $query = "SELECT * FROM projects";
+                    $result = $link->query($query);
+                    while($row = mysqli_fetch_array($result)){
+                        echo "<li><a href='$row[3]'>$row[1]</a></li>";
+                    }
+                    ?>
+                </ul>
+            </li>
             <li class="has-form">
                 <div class="row collapse">
                     <div class="large-8 small-9 columns">
@@ -39,64 +51,41 @@
     </section>
 </nav>
 <br>
+
 <div class="row">
     <div class="large-12 column">
         <img src="img/hd1080.png">
     </div>
 </div>
 <br>
+
 <div class="row section1 sections">
-    <br>
+
     <div class="large-offset-1 column left-align">
         <h1>About</h1>
     </div>
-    <br>
-    <br>
-    <br>
-    <br>
+
     <div class="large-offset-2">
-        <?php
-        $link = mysqli_connect('localhost','public','XZmMSa7fZHfR5a8C','main') or die("error ". mysqli_error($link));
-        $query = "SELECT * FROM projects";
-        $result = $link->query($query);
-        while($row = mysqli_fetch_array($result)){
-            echo $row["id"]."<br>";
-        }
-        ?>
-        <p>
-            <?php
-            for( $i=0; $i<5; $i++){
-            echo "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis suscipit sagittis. Quisque posuere massa elit, sed gravida arcu suscipit id. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas ac augue ut ex ultricies varius sit amet sit amet lorem. In at dui dignissim, fringilla elit eu, accumsan libero. Sed porttitor sollicitudin arcu, nec aliquam urna luctus ac. Integer vestibulum, augue ut volutpat bibendum, massa nibh viverra turpis, ut fermentum sapien felis vitae ligula. Donec in mattis quam. Etiam tincidunt tempus est in viverra.";
-            }
-            ?>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis suscipit sagittis. Quisque posuere massa elit, sed gravida arcu suscipit id. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas ac augue ut ex ultricies varius sit amet sit amet lorem. In at dui dignissim, fringilla elit eu, accumsan libero. Sed porttitor sollicitudin arcu, nec aliquam urna luctus ac. Integer vestibulum, augue ut volutpat bibendum, massa nibh viverra turpis, ut fermentum sapien felis vitae ligula. Donec in mattis quam. Etiam tincidunt tempus est in viverra.
-        </p>
+
     </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+
 </div>
 <div class="row section2 sections">
     <div class="large-offset-1 column left-align">
         <h1>What we do...</h1>
     </div>
-    <br>
-    <br>
-    <br>
+
     <div class="large-offset-2">
         <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis suscipit sagittis. Quisque posuere massa elit, sed gravida arcu suscipit id. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Maecenas ac augue ut ex ultricies varius sit amet sit amet lorem. In at dui dignissim, fringilla elit eu, accumsan libero. Sed porttitor sollicitudin arcu, nec aliquam urna luctus ac. Integer vestibulum, augue ut volutpat bibendum, massa nibh viverra turpis, ut fermentum sapien felis vitae ligula. Donec in mattis quam. Etiam tincidunt tempus est in viverra.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sagittis suscipit sagittis. Quisque posuere
+            massa elit, sed gravida arcu suscipit id. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+            posuere cubilia Curae; Maecenas ac augue ut ex ultricies varius sit amet sit amet lorem. In at dui
+            dignissim, fringilla elit eu, accumsan libero. Sed porttitor sollicitudin arcu, nec aliquam urna luctus ac.
+            Integer vestibulum, augue ut volutpat bibendum, massa nibh viverra turpis, ut fermentum sapien felis vitae
+            ligula. Donec in mattis quam. Etiam tincidunt tempus est in viverra.
         </p>
     </div>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
-    <br>
+
 </div>
 <!--<div class="row" data-equalizer data-equalizer-mq="large-up">
     <div></div>
