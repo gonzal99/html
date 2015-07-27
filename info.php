080c7c163eebbbd4e48abbcb1aab4015d2545e3a
gonzal99 <gonzal99@students.rowan.edu>
2015-07-27 10:10:34 -0400

    check if https isset

--- info.php
+++ info.php
@@ -5,8 +5,9 @@
  * Date: 7/27/2015
  * Time: 10:01 AM
  */
-if($_SERVER['https']!= null){
+if($_SERVER['HTTPS']!= null){
     echo "https on";
 }else{
     echo "https off";
 }
+echo isset($_SERVER['HTTPS']);
