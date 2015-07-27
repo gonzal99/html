016d781ed448e7619e541e6888d7587e890235af
gonzal99 <gonzal99@students.rowan.edu>
2015-07-27 13:20:43 -0400

    fill page

--- projects/mmserver/index.php
+++ projects/mmserver/index.php
@@ -16,8 +16,8 @@ if (!isset($_SERVER['HTTPS'])) {
     <script src="js/displayHelper.js"></script>
     <script src="js/postHelper.js"></script>
 </head>
-<body style="min-height: 100% !important;">
-<div class="off-canvas-wrap" data-offcanvas>
+<body>
+<div class="off-canvas-wrap fillPage" data-offcanvas>
     <div class="inner-wrap">
         <nav class="tab-bar">
             <section class="left-small">
