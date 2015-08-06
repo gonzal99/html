d2cbfb706da86adb5c278a70280210241c1954c6
Roberto Gonzalez <gonzal99@students.rowan.edu>
2015-08-05 22:08:54 -0400

    pricing table

--- projects/mmserver/php/donate.php
+++ projects/mmserver/php/donate.php
@@ -7,33 +7,42 @@
  */
 session_start();
 ?>
-<div class="row panel">
+<div class="row panel" align="center">
     <ul class="pricing-table">
-        <li class="title">Monthly</li>
-        <li class="price">$1.99</li>
-        <li class="title">Yearly</li>
-        <li class="price">$19.99</li>
-        <li class="description">Save $3.99 by subscribing for a year!</li>
+        <div class="row">
+            <div class="large-12 column">
+                <li class="title">Monthly</li>
+                <li class="price">$1.99</li>
+            </div>
+        </div>
+        <div class="row">
+            <div class="large-12 column">
+                <li class="title">Yearly</li>
+                <li class="price">$19.99</li>
+                <li class="description">Save $3.99 by subscribing for a year!</li>
+            </div>
+        </div>
     </ul>
-
-    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-        <input type="hidden" name="cmd" value="_s-xclick">
-        <input type="hidden" name="hosted_button_id" value="LRCB3B7GYVE3G">
-        <table>
-            <tr>
-                <td><input type="hidden" name="on0" value=""></td>
-            </tr>
-            <tr>
-                <td><select name="os0">
-                        <option value="Monthly">Monthly : $1.99 USD - monthly</option>
-                        <option value="Yearly">Yearly : $19.99 USD - yearly</option>
-                    </select></td>
-            </tr>
-        </table>
-        <input type="hidden" name="currency_code" value="USD">
-        <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0"
-               name="submit" alt="PayPal - The safer, easier way to pay online!">
-        <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-    </form>
+    <div class="large-12 column">
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="LRCB3B7GYVE3G">
+            <table>
+                <tr>
+                    <td><input type="hidden" name="on0" value=""></td>
+                </tr>
+                <tr>
+                    <td><select name="os0">
+                            <option value="Monthly">Monthly : $1.99 USD - monthly</option>
+                            <option value="Yearly">Yearly : $19.99 USD - yearly</option>
+                        </select></td>
+                </tr>
+            </table>
+            <input type="hidden" name="currency_code" value="USD">
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0"
+                   name="submit" alt="PayPal - The safer, easier way to pay online!">
+            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+        </form>
+    </div>
 </div>
 
